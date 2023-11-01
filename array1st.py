bf79b304da9dc057d02271f4a2150b300c7980f1
Avijit sarkar <avhijitsarkar3@gmail.com>
2023-11-01 12:14:29 +0530

    new

---
+++ array1st.py
@@ -0,0 +1,19 @@
+# Implementation and declear of an array
+cars=["BMW","Volvo","Marcerse"]
+x= cars[0]
+x1= cars[1]
+x2= cars[2]
+print("Arrays declear :- ")
+print (x)
+print (x1)
+print (x2)
+# length of an array
+l=len(x1)
+print (l)
+#add new array
+cars.append("farrari")
+cars.pop(2)
+cars.remove("Volvo")
+print (cars)
+for z in cars:
+    print (z)
---
+++ lambda.py
@@ -0,0 +1,12 @@
+#1 argument for lambda
+x=lambda a : a+10
+print(x(5))
+print(x(4))
+print(x(3))
+#multipal argument 
+x=lambda a,b : a*b
+print(x(7,8))
+
+#summrize arguments
+x=lambda a,b,c : a + b + c
+print(x,(5,6,2))
